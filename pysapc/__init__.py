e13a85704e65d5ee7e87985eea68157e3b00776d
bioinfocao <bioinfocao@gmail.com>
2020-05-12 23:16:51 -0500

    Add files via upload

--- pysapc/__init__.py
+++ pysapc/__init__.py
@@ -1,3 +1,6 @@
+# For relative imports to work in Python 3.6
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
 from SparseAPCluster import SAP
 from SparseAPCluster import sparseAffinityPropagation
 import sparseAP_cy
