a3f4bac26037e2e9ad7b803d7a3d1d735758451c
bioinfocao <bioinfocao@gmail.com>
2020-05-12 23:10:37 -0500

    Add files via upload

--- pysapc/tests/__init__.py
+++ pysapc/tests/__init__.py
@@ -1,2 +1,5 @@
+# For relative imports to work in Python 3.6
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
 from test_sap import testDense
 from test_sap import testSparse
